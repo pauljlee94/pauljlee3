bf30ae12bbaed456a19db246ca7c9b224e96886f
Paul Lee <pauljlee94@gmail.com>
2020-10-22 18:27:55 -0400

    prismic

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,14 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    "@nuxtjs/prismic"
   ],
+
+  prismic: {
+    endpoint: 'https://paulspersonalwebsite.cdn.prismic.io/api/v2',
+    linkResolver: '@/plugins/link-resolver',
+    htmlSerializer: '@/plugins/html-serializer',
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
