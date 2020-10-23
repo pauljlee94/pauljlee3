4106ba3dae75753dce98edfd3b5650417f0da826
Paul Lee <pauljlee94@gmail.com>
2020-10-23 18:37:47 -0400

    form added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="bg-black text-white font-work">
-    <header class="fixed p-6">
+    <header class="fixed p-6 z-10 blend">
       <nav class="flex flex-col">
-        <a class="main-nav-item" href="#about">About</a>
-        <a class="main-nav-item" href="#work">Work</a>
-        <a class="main-nav-item" href="#contact">Contact</a>
+        <nuxt-link class="main-nav-item" to="/#about">
+          <span v-in-view class="underline">About</span>
+          <!-- <span :class="[$route.hash.includes('about') ? 'active' : null, 'fade-in']">•</span> -->
+        </nuxt-link>
+        <nuxt-link class="main-nav-item" to="/#work">
+          <span class="underline">Work</span> 
+          <!-- <span :class="[$route.hash.includes('work') ? 'active' : null, 'fade-in']">•</span> -->
+        </nuxt-link>
+        <nuxt-link class="main-nav-item" to="/#contact">
+          <span class="underline">Contact</span>
+          <!-- <span :class="[$route.hash.includes('contact') ? 'active' : null, 'fade-in']">•</span> -->
+        </nuxt-link>
       </nav>
     </header>
     <main>
@@ -12,3 +21,4 @@
     </main>
   </div>
 </template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,15 @@
     <!-- Intro -->
     <section class="h-screen flex flex-col justify-between pt-40 pb-6 px-6 text-right">
       <h1 class="text-6xl font-bold flex flex-col text-2xl leading-tight blend" v-html="home.tagline[0].text"></h1>
-      <p class="flex flex-col text-lg blend">
+      <p class="flex flex-col text-lg">
         <span> PAUL J. LEE</span>
         <span>{{ new Date().getFullYear() }} PORTFOLIO</span>
       </p>
     </section>
 
     <!-- About -->
-    <section id="about" class="h-screen px-6 pt-40">
-      <div class="box blend" v-html="home.about[0].text">
-      </div>
+    <section id="about" class="h-screen px-6 pt-48">
+      <div  v-scroll class="box p-6 blend fade-up" v-html="home.about[0].text"></div>
     </section>
 
     <!-- Work -->
@@ -29,8 +28,36 @@
     </section>
 
     <!-- Contact -->
-    <section id="contact" class="h-screen pt-40 px-6">
-      <div>contact</div>
+    <section id="contact" class="h-screen pt-48 pb-20 px-6 max-w-2xl mx-auto">
+      <form action="https://formspree.io/f/xgepkrjg"  method="POST" class="flex flex-col justify-between">
+        <div>
+          <label for="name" class="block text-lg">
+            Name
+          </label>
+          <div class="mt-2 border-2 border-white p-2">
+            <input id="name" name="name" required class="bg-black block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
+          </div>
+        </div>
+        <div class="mt-3">
+          <label for="email" class="block text-lg">
+            Email
+          </label>
+          <div class="mt-2 border-2 border-white p-2">
+            <input id="email" name="email" required class="bg-black block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
+          </div>
+        </div>
+        <div class="mt-3">
+          <label for="message" class="block text-lg">
+            Message
+          </label>
+          <div class="mt-2 border-2 border-white p-2">
+            <textarea id="message" name="message" required class="h-64 bg-black resize-none block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
+          </div>
+        </div>
+        <div class="mt-6">
+          <button class="border-2 p-2 w-full text-lg">Send</button>
+        </div>
+      </form>
     </section>
 
     <!-- Footer -->
