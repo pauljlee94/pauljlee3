0d0be5c0c5876296c6ff3c6eae5615d6837619dc
Paul Lee <pauljlee94@gmail.com>
2020-10-26 16:54:00 -0400

    webfonts added

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,32 +12,26 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:wght@900&family=Work+Sans:wght@300;500&display=swap" }
     ]
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
   ],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '@/plugins/v-scroll'
   ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     "@nuxtjs/prismic",
     ['vue-scrollto/nuxt', {duration: 1000}],
+    'nuxt-webfontloader',
   ],
 
   prismic: {
@@ -46,36 +40,11 @@ export default {
     htmlSerializer: '@/plugins/html-serializer',
   },
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   },
-
-  router: {
-    // scrollBehavior: async (to, from, savedPosition) => {
-    //   if (savedPosition) {
-    //     return savedPosition
-    //   }
-
-    //   const findEl = async (hash, x) => {
-    //     return document.querySelector(hash) ||
-    //       new Promise((resolve, reject) => {
-    //         if (x > 50) {
-    //           return resolve()
-    //         }
-    //         setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 100)
-    //       })
-    //   }
-
-    //   if (to.hash) {
-    //     let el = await findEl(to.hash)
-    //     if ('scrollBehavior' in document.documentElement.style) {
-    //       return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
-    //     } else {
-    //       return window.scrollTo(0, el.offsetTop)
-    //     }
-    //   }
-
-    //   return { x: 0, y: 0 }
-    // }
-  }
+  webfontloader: {
+    google: {
+      families: ['Montserrat:900', 'Work Sans:400,500,700', 'Merriweather:400']
+    }
+  },
 }
